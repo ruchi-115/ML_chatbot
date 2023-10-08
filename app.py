c3fc4b3e9d20c94c50fc31ef3166cf4596a6460f
ruchi-115 <ruchitasonawale115@gmail.com>
2023-10-08 16:04:59 +0530

    delployment success

--- app.py
+++ app.py
@@ -27,12 +27,14 @@ function_descriptions = [
     }
 ]
 
+
 def get_subject_name(topic, operator):
     subject_info = {
         "topic": topic,
         "operator": operator,
     }
     return json.dumps(subject_info)
+
 
 def check_values_in_dict(dict1, dict2):
     # Extract the values from dict1
@@ -43,30 +45,61 @@ def check_values_in_dict(dict1, dict2):
     for valid_values in dict2.values():
         if topic_value in valid_values or operator_value in valid_values:
             return True
-        else: 
+        else:
             return False
 
 
 # Sample data with valid topics and operators
 data = {
     "valid_topics": [
-        "addition", "subtraction", "division", "multiplication", "degree",
-        "square", "cube", "cuboid", "cone", "cylinder", "circle", "triangle",
-        "parallelogram", "rectangle", "trapezium", "rhomboid", "rhombus"
+        "addition",
+        "subtraction",
+        "division",
+        "multiplication",
+        "degree",
+        "square",
+        "cube",
+        "cuboid",
+        "cone",
+        "cylinder",
+        "circle",
+        "triangle",
+        "parallelogram",
+        "rectangle",
+        "trapezium",
+        "rhomboid",
+        "rhombus",
     ],
     "valid_operators": [
-        "+", "-", "*", "/", "sin", "degree", "cos", "tan", "cot", "cosec",
-        "sec", "log", "area", "volume", "perimeter", "circumference",
-        "radius", "diameter"
-    ]
+        "+",
+        "-",
+        "*",
+        "/",
+        "sin",
+        "degree",
+        "cos",
+        "tan",
+        "cot",
+        "cosec",
+        "sec",
+        "log",
+        "area",
+        "volume",
+        "perimeter",
+        "circumference",
+        "radius",
+        "diameter",
+    ],
 }
 
 # Flask app setup
 app = Flask(__name__, template_folder="templates")
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/get", methods=["POST"])
 def get_response():
@@ -90,6 +123,7 @@ def get_response():
         )
         assistant_message = response.choices[0].message["content"]
         return jsonify({"assistant_message": assistant_message})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
